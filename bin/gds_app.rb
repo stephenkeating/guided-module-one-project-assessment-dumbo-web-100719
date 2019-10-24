bfe16d4882bcc6c00542889962fc2172a32f36c7
github username <github email address>
2019-10-24 18:54:57 -0400

    debugging

--- bin/gds_app.rb
+++ bin/gds_app.rb
@@ -9,7 +9,8 @@ class GdsApp
   @@all_characters_without_chosen_character
   @@open_order
   @@item_selected_name_and_price
-  @@current_delivery
+  # @@current_delivery
+  @@delivered_order
 
 #use a clear after each method
 
@@ -17,7 +18,12 @@ class GdsApp
     #put clear command here to clean up the CLI
     system "clear"
     puts "Hello and welcome to Ghibli Delivery Service!".green
-    prompt.keypress("Press Enter to Start", key: [:enter])
+    puts "
+    \nWarring armies are devastating the Forest! 
+    \nHeal the Forest by working together with your friends to gather magical seeds.
+    \nPlace and deliver orders to each other create more seeds.
+    \nLet's play!"
+    prompt.keypress("\nPress [Enter] to Start", key: [:enter])
     choose_character_page
   end
 
@@ -34,12 +40,13 @@ class GdsApp
 
   def self.character_page #need to display seeds and deliveries
     system "clear"
-    puts "Hi" + " #{@@chosen_character.name.rstrip}!!".yellow + "\nYou have " + "#{@@chosen_character.seeds} seeds".blue + " & " + "#{@@chosen_character.orders_as_deliverer.count} order(s)".green + " to deliver!"
-    selection = prompt.select("What would you like to do?") do |menu|
+    #local_chosen_character = @@chosen_character
+    #updated_chosen_char_count = local_chosen_character.orders_as_deliverer.where(status: "Ready to Deliver").count
+    puts "Hi" + " #{@@chosen_character.name.rstrip}!!\n".yellow + "\nYou have " + "#{@@chosen_character.seeds} seeds".blue + " & " + "#{@@chosen_character.orders_as_deliverer.count} order(s)".green + " to deliver!"
+    selection = prompt.select("\nWhat would you like to do?") do |menu|
       menu.choice name: 'New Order',  value: 1
       menu.choice name: 'Deliver an Order', value: 2
-      menu.choice name: 'View History',  value: 3
-      menu.choice name: 'Go back to Charater Menu',  value: 4
+      menu.choice name: 'Go back to Charater Menu',  value: 3
     end
     if selection == 1
       #binding.pry
@@ -62,7 +69,7 @@ class GdsApp
     # #OrderItem.create(order_id: )
 
     items_list = Item.name_and_price
-    @@item_selected_name_and_price = prompt.select("Hi " + "#{@@chosen_character.name.rstrip}".yellow + ". Press enter to add item and proceed to checkout.\n" + "Items:       Prices:", items_list, per_page: 10)
+    @@item_selected_name_and_price = prompt.select("#{@@chosen_character.name.rstrip}".yellow + ", what would you like to order?\n" + "\nPress [Enter] to add item and proceed to Checkout.\n" + "\nItems:   |   Prices:" + "\n-----------------------\n", items_list, per_page: 10)
     selected_item_name =  @@item_selected_name_and_price.slice(10...18)
     @@item_selected_for_checkout = Item.find_by_name(selected_item_name)
     OrderItem.create(order_id: @@open_order.id, item_id: @@item_selected_for_checkout.id)
@@ -75,7 +82,7 @@ class GdsApp
   def self.checkout_page
     system "clear"
     current_char = @@chosen_character
-    puts "Hi " + "#{@@chosen_character.name.rstrip}".yellow + ", here is your order!\n" + "\nCart" + "\n-----------------------------\n"+ "Item:  Price:     " + "\n-----------------------------\n" + "#{@@item_selected_name_and_price}"
+    puts "Here is your order " + "#{@@chosen_character.name.rstrip}".yellow + ".\n" + "\nCart" + "\n-----------------------------\n"+ "Item:  |  Price:     " + "\n-----------------------------\n" + "#{@@item_selected_name_and_price}"
   
     selection = prompt.select("\nWhat would you like to do?") do |menu|
       menu.choice name: 'Place Order',  value: 1
@@ -88,8 +95,8 @@ class GdsApp
       current_order.update(status: "Ready for Delivery")
       loading_pending_order_page
     elsif selection == 2
-      @@open_order.customer_seed_cost = 0
-      @@open_order.deliverer_seed_payout = 0
+      @@open_order.customer_seed_cost = 0   #change to local variable?
+      @@open_order.deliverer_seed_payout = 0 #change to local variable?  
       new_order_page
     elsif selection == 3
       Order.where(id: current_order.id).destroy_all
@@ -102,35 +109,44 @@ class GdsApp
     #put an image here and bring back to main menu; say ___is delivering your order
     system "clear"
     current_order = @@open_order
-    puts "#{current_order.deliverer.name.rstrip} is currently delivering your order!"
-    prompt.keypress("Press Enter to Return to the Character Selection Screen", key: [:enter])
+    puts "#{current_order.deliverer.name.rstrip}".yellow + " will be delivering your order!\n"
+    prompt.keypress("\nPress [Enter] to return to Character Selection menu.", key: [:enter])
     choose_character_page
   end
 
   def self.deliver_order_page
     system "clear"
     current_char = @@chosen_character
-    order_count = current_char.orders_as_deliverer.count
-    puts "Welcome #{current_char.name.rstrip}, you currently have #{order_count} order(s) to deliver."
+    order_count = current_char.orders_as_deliverer.count 
+    if order_count == 0
+      prompt.keypress("You currently have no deliveries. Press [Enter] to return to Character Selection menu.", key: [:enter])
+      choose_character_page
+    else 
+    puts "Welcome " + "#{current_char.name.rstrip}".yellow + ", you currently have" + " #{order_count} order(s)".green + " to deliver."
 
     # pull hash of all available orders for the deliverer with string as key, and order instance as value. 
     # add payout to current_user's seed bank.
     # is there a way to add an additional menu item after the array that generated a bunch of menu items? yes == we'll need an if then before we do prompt.select
 
     #current_char.my_orders_as_deliverer_hash
-    selection = prompt.select("Choose an order to deliver.", current_char.my_orders_as_deliverer_hash)
-    @@current_delivery = selection
-    current_char.seeds += selection.deliverer_seed_payout
+    # "Choose your character!\n\nCharacter  | " + "Seeds ".blue + "| " + "Orders to Deliver".green + "\n--------------------------------\n", char_list, per_page: 10)
+    #  binding.pry
+    selection = prompt.select("\nChoose which order you want to deliver to get more seeds!\n" + "\nRecepient".yellow + "   |   " +  "Award".blue + "\n---------------------\n", current_char.my_orders_as_deliverer_hash)
+    # @@current_delivery = selection
+    current_char.update(seeds: current_char.seeds + selection.deliverer_seed_payout)
     selection.update(status: "Delivered")
+    # @@delivered_order = @@current_delivery
+    @@delivered_order = selection
     delivery_splash_page
+    end 
   end
 
   def self.delivery_splash_page
     system "clear"
-    current_del = @@current_delivery
+    current_del = @@delivered_order
     current_char = @@chosen_character
-    puts "You delivered #{current_del.customer.name.rstrip}'s order! You now what #{current_char.seeds} seed(s)."# Yay, you're one step closer to saving the forest"
-    prompt.keypress("Press Enter to return to your Character Screen", key: [:enter])
+    puts "You delivered #{current_del.customer.name.rstrip.yellow}'s order! You now have " + "#{current_char.seeds} seed(s)".blue + "."# Yay, you're one step closer to saving the forest"
+    prompt.keypress("\nPress [Enter] to return to your Character Screen", key: [:enter])
     choose_character_page
   end
 
